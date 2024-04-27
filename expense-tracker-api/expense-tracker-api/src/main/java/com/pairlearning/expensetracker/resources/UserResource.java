6751b74b263c1ae77723aa32a5822522b5d68c2a
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2024-04-27 20:16:54 +0100

    Password encryption & JWT authorisation

--- expense-tracker-api/expense-tracker-api/src/main/java/com/pairlearning/expensetracker/resources/UserResource.java
+++ expense-tracker-api/expense-tracker-api/src/main/java/com/pairlearning/expensetracker/resources/UserResource.java
@@ -3,6 +3,11 @@ package com.pairlearning.expensetracker.resources;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pairlearning.expensetracker.services.UserService;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import com.pairlearning.expensetracker.Constants;
 import com.pairlearning.expensetracker.domain.User;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
+import java.util.Date;
 import java.util.HashMap;
 
 @RestController
@@ -22,6 +28,17 @@ public class UserResource {
     @Autowired
     UserService userService;
 
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> loginUser(@RequestBody Map<String, Object> userMap) {
+        String email = (String) userMap.get("email");
+        String password = (String) userMap.get("password");
+
+        User user = userService.validateUser(email, password);
+
+        return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
+    }
+    
+
     @PostMapping("/register")
     public ResponseEntity<Map<String, String>> registerUser(@RequestBody Map<String, Object> userMap) {
         String firstName = (String) userMap.get("firstName");
@@ -30,8 +47,22 @@ public class UserResource {
         String password = (String) userMap.get("password");
 
         User user = userService.registerUser(firstName, lastName, email, password);
+      
+        return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
+    }
+
+    private Map<String, String> generateJWTToken(User user) {
+        long timestamp = System.currentTimeMillis();
+        String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
+                .setIssuedAt(new Date(timestamp))
+                .setExpiration(new Date(timestamp + Constants.TOKEN_VALIDITY))
+                .claim("userID", user.getUserId())
+                .claim("email", user.getEmail())
+                .claim("firstName", user.getFirstName())
+                .claim("lastName", user.getLastName())
+                .compact();
         Map<String, String> map = new HashMap<>();
-        map.put("message", "registerd successfully");
-        return new ResponseEntity<>(map, HttpStatus.OK);
+        map.put("token", token);
+        return map;
     }
 }
--- expense-tracker-api/expense-tracker-api/src/main/java/com/pairlearning/expensetracker/services/UserServiceImpl.java
+++ expense-tracker-api/expense-tracker-api/src/main/java/com/pairlearning/expensetracker/services/UserServiceImpl.java
@@ -33,8 +33,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User validateUser(String email, String password) throws EtAuthException {
-        // TODO Auto-generated method stub
-        return null;
+        if(email != null) email = email.toLowerCase();
+        return userRepository.findByEmailAndPassword(email, password);
     }
     
 }
